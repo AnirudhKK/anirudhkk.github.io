33d9d19673a772e7887d5f1c2b8c2cc957768575
AnirudhKK <iamanirudh.ak@gmail.com>
2020-07-23 17:13:39 +0530

    update

--- action_page.php
+++
@@ -1,7 +0,0 @@
-<?php
-$fp = fopen("data.txt", "a") or die("Unable to open file!");
-$data = $_POST['name'] . '-' . $_POST['email'] . "\r\n" . '-' . $_POST['mobile'];
-fwrite($fp, $data);
-fclose($fp);
-header('Location: contact.html');
-?>
