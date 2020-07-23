41b80b98d1a77c1713ccd39b3ede44559551982e
AnirudhKK <iamanirudh.ak@gmail.com>
2020-07-23 16:56:24 +0530

    UPDATE

--- action_page.php
+++ action_page.php
@@ -1,28 +1,7 @@
 <?php
-
-if(isset($_POST['submit'])) {
-$myemail='iamanirudh,ak@gmail.com'
-$name = $_POST['name']; 
-$email_address = $_POST['email']; 
-$mobile_number = $_POST['mobile']
-$message = $_POST['message'];
-
-$to = $myemail;
-
-$email_subject = "Contact form submission: $name";
-
-$email_body = "You have received a new message. ".
-
-" Here are the details:\n Name: $name \n ".
-"Mobile No. : $mobile\n".
-"Email: $email_address\n Message \n $message";
-
-$headers = "From: $myemail\n";
-
-$headers .= "Reply-To: $email_address";
-
-mail($to,$email_subject,$email_body,$headers);
-
+$fp = fopen("data.txt", "a") or die("Unable to open file!");
+$data = $_POST['name'] . '-' . $_POST['email'] . "\r\n" . '-' . $_POST['mobile'];
+fwrite($fp, $data);
+fclose($fp);
 header('Location: contact.html');
-}
 ?>
