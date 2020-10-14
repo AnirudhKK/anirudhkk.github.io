a25ac4d4ff06a286544759bcd37ecf9a56d3911c
AnirudhKK <iamanirudh.ak@gmail.com>
2020-10-14 18:45:57 +0530

    redesign

---
+++ index.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+  // Transition effect for navbar
+  $(window).scroll(function () {
+    // checks if window is scrolled more than 500px, adds/removes solid class
+    if ($(this).scrollTop() > 100) {
+      $(".navbar").addClass("solid");
+    } else {
+      $(".navbar").removeClass("solid");
+    }
+  });
+});
+
+
+function handleMenu() {
+  if($("ul").css("display")==="none"){
+  $("ul").css("display","flex");
+  }
+  else if($("ul").css("display")==="flex"){
+    $("ul").css("display","none");
+  }
+  
+  $( ".hamburger" ).toggle();
+  $( ".cross" ).toggle();
+}
